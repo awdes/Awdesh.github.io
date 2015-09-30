870676e6a02e2217578a134b522dc1784ce47190
Awdesh Sharma <awdesh.sharma1@gmail.com>
2015-09-29 22:58:54 -0500

    Committing Word Puzzle Problem

---
+++ WordPuzzle/WordPuzzle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Client class responsible for calling Solve
+/// method of the puzzle solver.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Main method that initiates the program.
+    /// </summary>
+    static void Main()
+    {
+        WordPuzzle.PuzzleSolver.Solve();
+    }
+}
