1ff40839d0649864b839dcf5a9d2c67e2e18408a
awdesh <awdesh.sharma1@gmail.com>
2015-12-10 23:46:25 -0600

    Adding gulp file and mocha test.

--- MEAN app/index.js
+++ MEAN app/index.js
@@ -51,8 +51,10 @@
 
 
 var fn = require('./myFile');
-
 fn();
 
 var otherFn = require('./test').other;
 otherFn();
+
+var newFn = require('./test').newFile;
+newFn();
--- MEAN app/test/index.js
+++ MEAN app/test/index.js
@@ -1,1 +1,2 @@
 exports.other = require('./myOtherFile');
+exports.newFile = require('./newFile');
